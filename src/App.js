0747e0cbe826753f454b2f49e562b0690d148f68
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-07-07 00:12:52 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Header(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  return (
+    <nav>
+      <a
+        href="/main"
+        onClick={(event) => {
+          event.preventDefault();
+          props.onchangeMode('MAIN');
+        }}
+      >
+        메인
+      </a>{' '}
+      |
+      <a
+        href="/my"
+        onClick={(event) => {
+          event.preventDefault();
+          props.onchangeMode('MY');
+        }}
+      >
+        마이 페이지
+      </a>{' '}
+      |
+      <a href="#">로그인</a> |
+      <a href="#">회원가입</a>
+    </nav>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <ul>
+        <li>
+          <a href="#">공연장 정보</a>
+        </li>
+        <li>
+          <a href="#">뮤지컬 순위</a>
+        </li>
+        <li>
+          <a href="#">연극 순위</a>
+        </li>
+        <li>
+          <a href="#">전체 공연(인기순 나열)</a>
+        </li>
+      </ul>
+    </main>
+  );
+}
+
+function My() {
+  return (
+    <my>
+      <ul>
+        <li>
+          <a href="#">내 일정 페이지(직접 등록)</a>
+        </li>
+        <li>
+          <a href="#">관심 배우 일정 페이지</a>
+        </li>
+        <li>
+          <a href="#">연극 순위</a>
+        </li>
+      </ul>
+    </my>
+  );
+}
+
+function App() {
+  const [mode, setMode] = useState('TOP_MENU');
+  let content = null;
+
+  if (mode === 'TOP_MENU') {
+    content = null;
+  } else if (mode === 'MAIN') {
+    content = <Main />;
+  } else if (mode === 'MY') {
+    content = <My />;
+  }
+
+  const onchangeMode = (newMode) => {
+    setMode(mode === newMode ? 'TOP_MENU' : newMode);
+  };
+
+  return (
+    <div>
+      <Header title="a.pm" />
+      <Nav id="top_menu" onchangeMode={onchangeMode} />
+      {content}
     </div>
   );
 }
